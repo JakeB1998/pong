7926d1bcfd7c9131b6607dbdb7866a3502534d49
JakeB1998 <49763318+JakeB1998@users.noreply.github.com>
2020-12-19 14:00:54 -0600

    Code cleanup

--- src/main/org/botka/pong/GameController.java
+++ src/main/org/botka/pong/GameController.java
@@ -168,223 +168,6 @@ public final class GameController  {
 		mGC.clearRect(0, 0, 2000, 2000);
 	}
 	
-	
-	/**
-	 * Initializes FXMl loading.
-	 */
-	@FXML
-	public void initialize() {
-		mRestartMenuItem.setOnAction(new RestartBtnAction());
-		mExitMenuItem.setOnAction(new ExitBtnAction());
-		mCanvas = new Canvas(1500, 1000);
-		mGC = mCanvas.getGraphicsContext2D();
-
-		mGamePane.getChildren().add(mCanvas);
-		
-
-		mPlayerOne = new Player(KeyCode.W, KeyCode.S, Side.Left);
-		mPlayerTwo = new Player(KeyCode.P, KeyCode.L, Side.Right);
-		
-		mGameBall = new Ball(mGamePane);
-		mGameBall.setVisible(false);
-		mGameBall.setDisable(true);
-
-		mPlayerOnePaddle = mPlayerOne.getPlayerPaddle().getPaddle();
-		mPlayerTwoPaddle = mPlayerTwo.getPlayerPaddle().getPaddle();
-
-		mGamePane.getChildren().add(mPlayerOne.getPlayerPaddle().getPaddle());
-		mGamePane.getChildren().add(mPlayerTwo.getPlayerPaddle().getPaddle());
-		mGamePane.getChildren().add(mGameBall);
-		//Ensures that canvas is visible
-		//mCanvas.toFront();
-
-		
-		
-		System.out.println(mGamePane.getChildren().toString());
-		this.updaterInit();
-	}
-
-	public void initalizeLayout() {
-		mPlayerOnePaddle.setLayoutX((mGamePane.getLayoutBounds().getMinX() + 100));
-		mPlayerOnePaddle.setLayoutY(mGamePane.getLayoutBounds().getMaxY() / 2);
-		mPlayerTwoPaddle.setLayoutX(mGamePane.getLayoutBounds().getMaxX() - 100);
-		mPlayerTwoPaddle.setLayoutY(mGamePane.getLayoutBounds().getMaxY() / 2);
-		mPlayerOne.getPlayerPaddle().setCord(new Point2D(mPlayerOnePaddle.getLayoutX(), mPlayerOnePaddle.getLayoutY()));
-		mPlayerTwo.getPlayerPaddle().setCord(new Point2D(mPlayerTwoPaddle.getLayoutX(), mPlayerTwoPaddle.getLayoutY()));
-		//Adds boundry lines for the screen edges to check for collisions.
-		mScreenLineOne = new Line();
-		mScreenLineOne.setStartX(0);
-		mScreenLineOne.setEndX(mGamePane.getLayoutBounds().getMaxX());
-		mScreenLineOne.setLayoutX(0);
-		mScreenLineOne.setLayoutY(mGamePane.getLayoutBounds().getMaxY());
-		mScreenLineTwo = new Line();
-		mScreenLineTwo.setStartX(0);
-		mScreenLineTwo.setEndX(mGamePane.getLayoutBounds().getMaxX());
-		mScreenLineTwo.setLayoutX(0);
-		mScreenLineTwo.setLayoutY(mGamePane.getLayoutBounds().getMinY());
-		
-		mGamePane.getChildren().add(mScreenLineTwo);
-		
-		mPlayerOneGoalLine = new Line();
-		mPlayerOneGoalLine.setLayoutX(0);
-		mPlayerOneGoalLine.setLayoutY(0);
-		mPlayerOneGoalLine.setStartY(mGamePane.getLayoutBounds().getMinY());
-		mPlayerOneGoalLine.setEndY(mGamePane.getLayoutBounds().getMaxY());
-		mPlayerTwoGoalLine = new Line();
-		mPlayerTwoGoalLine.setLayoutX(mGamePane.getLayoutBounds().getMaxX());
-		mPlayerTwoGoalLine.setLayoutY(0);
-		mPlayerTwoGoalLine.setStartY(mGamePane.getLayoutBounds().getMinY());
-		mPlayerTwoGoalLine.setEndY(mGamePane.getLayoutBounds().getMaxY());
-		mScreenLineOneBounds = mScreenLineOne.getBoundsInParent();
-		mScreenLineTwoBounds = mScreenLineTwo.getBoundsInParent();
-		mPlayerOneGoalLineBounds = mPlayerOneGoalLine.getBoundsInParent();
-		mPlayerTwoGoalLineBounds = mPlayerTwoGoalLine.getBoundsInParent();
-
-		/*
-		mScoreLabelTwo.relocate(mGamePane.getLayoutBounds().getMaxX() / 2 - 50, mGamePane.getLayoutBounds().getMaxY() - 100);
-		mScoreLabelTwo.setFont(Font.font(50));
-
-		mScoreLabelOne.relocate(mGamePane.getLayoutBounds().getMaxX() / 2 + 50, mGamePane.getLayoutBounds().getMaxY() - 100);
-		mScoreLabelOne.setFont(Font.font(50));
-		*/
-
-		mGamePane.getChildren().add(mPlayerOneGoalLine);
-		mGamePane.getChildren().add(mPlayerTwoGoalLine);
-	}
-	/**
-	 * Called after FXML initalization to pass scene to register listeners that can only be tagged onto the scene.
-	 * @param scene
-	 */
-	public void registerScene(Scene scene) {
-		KeyControl<KeyCode> playerOnePaddleUpControl = (KeyControl<KeyCode>)mPlayerOne.getPaddleUpControl();
-		KeyControl<KeyCode> playerOnePaddleDownControl = (KeyControl<KeyCode>)mPlayerOne.getPaddleDownControl();
-		KeyControl<KeyCode> playerTwoPaddleUpControl = (KeyControl<KeyCode>)mPlayerTwo.getPaddleUpControl();
-		KeyControl<KeyCode> playerTwoPaddleDownControl = (KeyControl<KeyCode>)mPlayerTwo.getPaddleDownControl();
-		mScene = scene;
-		mScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			
-			public void handle(KeyEvent event) {
-				System.out.println(event.getCharacter());
-				if (event.getCode().equals(playerOnePaddleUpControl.getControl())) {
-					if (mPlayerOneUp != true) {
-						playerOnePaddleUpControl.sendControlActivatedEvent(playerOnePaddleUpControl.getControl(), System.currentTimeMillis());
-						mPlayerOneUp = true;
-					}
-
-				} else if (event.getCode().equals(playerOnePaddleDownControl.getControl())) {
-					if (mPlayerOneDown != true) {
-						playerOnePaddleDownControl.sendControlActivatedEvent(playerOnePaddleDownControl.getControl(), System.currentTimeMillis());
-						mPlayerOneDown = true;
-					}
-
-				}
-				if (event.getCode().equals(playerTwoPaddleUpControl.getControl())) {
-					if (mPlayerTwoUp != true) {
-						playerTwoPaddleUpControl.sendControlActivatedEvent(playerTwoPaddleUpControl.getControl(), System.currentTimeMillis());
-						mPlayerTwoUp = true;
-					}
-
-				} else if (event.getCode().equals(playerTwoPaddleDownControl.getControl())) {
-					if (mPlayerTwoDown != true) {
-						playerTwoPaddleDownControl.sendControlActivatedEvent(playerTwoPaddleDownControl.getControl(), System.currentTimeMillis());
-						mPlayerTwoDown = true;
-					}
-				}
-			}
-		});
-		
-		mScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
-			public void handle(KeyEvent event) {
-				if (event.getCode().equals(playerOnePaddleUpControl.getControl())) {
-					mPlayerOneUp = false;
-				} else if (event.getCode().equals(playerOnePaddleDownControl.getControl())) {
-					mPlayerOneDown = false;
-				}
-				if (event.getCode().equals(playerTwoPaddleUpControl.getControl())) {
-					mPlayerTwoUp = false;
-				} else if (event.getCode().equals(playerTwoPaddleDownControl.getControl())) {
-					mPlayerTwoDown = false;
-				}
-			}
-		});
-	}
-	/**
-	 * Initializes the updater.
-	 */
-	public void updaterInit() {
-		
-		mGameLogicUpdater = new Timeline(
-				new KeyFrame(Duration.seconds(FRAME_PER_SECOND), new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent e) {
-						render();
-						if (mGameBall.isCanMove() == true) {
-							mGameBall.constantMove(mGameBall.getCenterX(), mGameBall.getCenterY(), mGC);
-							mGC.clearRect(0, 0, 2000, 2000);
-							mGameBall.setCenterX(mGameBall.getXLocation());
-							mGameBall.setCenterY(mGameBall.getYLocation());
-							mGC.fillOval(mGameBall.getCenterX(), mGameBall.getCenterY(), mGameBall.getRadius() * 2,
-									mGameBall.getRadius() * 2);
-						}
-						checkCollisions();
-						if (mGameBall.getCenterX() < mGamePane.getLayoutBounds().getMinX()
-								|| mGameBall.getCenterX() > mGamePane.getLayoutBounds().getMinX()) {
-							// gc.clearRect(0, 0, 2000, 2000);
-							// gameBall.reset(gamePane);
-
-						} else if (mGameBall.getCenterY() < mGamePane.getLayoutBounds().getMinY()
-								|| mGameBall.getCenterY() > mGamePane.getLayoutBounds().getMinY()) {
-							// gc.clearRect(0, 0, 2000, 2000);
-							// gameBall.reset(gamePane);
-						}
-
-						if (mPlayerOneUp && mPlayerOne.getPlayerPaddle().canMoveUp()) {
-							mPlayerOne.getPlayerPaddle().moveUp();
-						} else {
-							mPlayerOneUp = false;
-						}
-						if (mPlayerOneDown  && mPlayerOne.getPlayerPaddle().canMoveDown()) {
-							mPlayerOne.getPlayerPaddle().moveDown();
-						} else {
-							mPlayerOneDown = false;
-						}
-						if (mPlayerTwoUp && mPlayerTwo.getPlayerPaddle().canMoveUp()) {
-							mPlayerTwo.getPlayerPaddle().moveUp();
-						} else {
-							mPlayerTwoUp = false;
-
-						}
-						if (mPlayerTwoDown && mPlayerTwo.getPlayerPaddle().canMoveDown()) {
-							mPlayerTwo.getPlayerPaddle().moveDown();
-						} else {
-							mPlayerTwoDown = false;
-						}
-
-						
-						FpsDisplay += 1;
-					}
-				}));
-		mGameLogicUpdater.setCycleCount(Animation.INDEFINITE);
-	}
-	
-	/**
-	 * Initializes the FPS display.
-	 */
-	public void initFPSDisplay() {
-		Timeline oneSec = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
-			@SuppressWarnings("deprecation")
-			@Override
-			public void handle(ActionEvent event) {
-				//System.out.println(FpsDisplay);
-				//mScoreLabelOne.setText(Integer.toString(FpsDisplay));
-				FpsDisplay = 0;
-
-			}
-		}));
-		oneSec.setCycleCount(Animation.INDEFINITE);
-		oneSec.play();
-	}
-	
 	/**
 	 * Checks for collisions.
 	 */
@@ -445,17 +228,129 @@ public final class GameController  {
 		} 
 	}
 	
+	
+	/**
+	 * Initializes FXMl loading.
+	 */
+	@FXML
+	public void initialize() {
+		mRestartMenuItem.setOnAction(new RestartBtnAction());
+		mExitMenuItem.setOnAction(new ExitBtnAction());
+		mCanvas = new Canvas(1500, 1000);
+		mGC = mCanvas.getGraphicsContext2D();
+
+		mGamePane.getChildren().add(mCanvas);
+		
+
+		mPlayerOne = new Player(KeyCode.W, KeyCode.S, Side.Left);
+		mPlayerTwo = new Player(KeyCode.P, KeyCode.L, Side.Right);
+		
+		mGameBall = new Ball(mGamePane);
+		mGameBall.setVisible(false);
+		mGameBall.setDisable(true);
+
+		mPlayerOnePaddle = mPlayerOne.getPlayerPaddle().getPaddle();
+		mPlayerTwoPaddle = mPlayerTwo.getPlayerPaddle().getPaddle();
+
+		mGamePane.getChildren().add(mPlayerOne.getPlayerPaddle().getPaddle());
+		mGamePane.getChildren().add(mPlayerTwo.getPlayerPaddle().getPaddle());
+		mGamePane.getChildren().add(mGameBall);
+		//Ensures that canvas is visible
+		//mCanvas.toFront();
+
+		
+		
+		System.out.println(mGamePane.getChildren().toString());
+		this.updaterInit();
+	}
+
+	/**
+	 * Initalizes layout of scene.
+	 * This must be called after the stage.show() method is invoked.
+	 */
+	public void initalizeLayout() {
+		mPlayerOnePaddle.setLayoutX((mGamePane.getLayoutBounds().getMinX() + 100));
+		mPlayerOnePaddle.setLayoutY(mGamePane.getLayoutBounds().getMaxY() / 2);
+		mPlayerTwoPaddle.setLayoutX(mGamePane.getLayoutBounds().getMaxX() - 100);
+		mPlayerTwoPaddle.setLayoutY(mGamePane.getLayoutBounds().getMaxY() / 2);
+		mPlayerOne.getPlayerPaddle().setCord(new Point2D(mPlayerOnePaddle.getLayoutX(), mPlayerOnePaddle.getLayoutY()));
+		mPlayerTwo.getPlayerPaddle().setCord(new Point2D(mPlayerTwoPaddle.getLayoutX(), mPlayerTwoPaddle.getLayoutY()));
+		//Adds boundry lines for the screen edges to check for collisions.
+		mScreenLineOne = new Line();
+		mScreenLineOne.setStartX(0);
+		mScreenLineOne.setEndX(mGamePane.getLayoutBounds().getMaxX());
+		mScreenLineOne.setLayoutX(0);
+		mScreenLineOne.setLayoutY(mGamePane.getLayoutBounds().getMaxY());
+		mScreenLineTwo = new Line();
+		mScreenLineTwo.setStartX(0);
+		mScreenLineTwo.setEndX(mGamePane.getLayoutBounds().getMaxX());
+		mScreenLineTwo.setLayoutX(0);
+		mScreenLineTwo.setLayoutY(mGamePane.getLayoutBounds().getMinY());
+		
+		mGamePane.getChildren().add(mScreenLineTwo);
+		
+		mPlayerOneGoalLine = new Line();
+		mPlayerOneGoalLine.setLayoutX(0);
+		mPlayerOneGoalLine.setLayoutY(0);
+		mPlayerOneGoalLine.setStartY(mGamePane.getLayoutBounds().getMinY());
+		mPlayerOneGoalLine.setEndY(mGamePane.getLayoutBounds().getMaxY());
+		mPlayerTwoGoalLine = new Line();
+		mPlayerTwoGoalLine.setLayoutX(mGamePane.getLayoutBounds().getMaxX());
+		mPlayerTwoGoalLine.setLayoutY(0);
+		mPlayerTwoGoalLine.setStartY(mGamePane.getLayoutBounds().getMinY());
+		mPlayerTwoGoalLine.setEndY(mGamePane.getLayoutBounds().getMaxY());
+		mScreenLineOneBounds = mScreenLineOne.getBoundsInParent();
+		mScreenLineTwoBounds = mScreenLineTwo.getBoundsInParent();
+		mPlayerOneGoalLineBounds = mPlayerOneGoalLine.getBoundsInParent();
+		mPlayerTwoGoalLineBounds = mPlayerTwoGoalLine.getBoundsInParent();
+		mGamePane.getChildren().add(mPlayerOneGoalLine);
+		mGamePane.getChildren().add(mPlayerTwoGoalLine);
+	}
+	
+	/**
+	 * Initializes the FPS display.
+	 */
+	public void initFPSDisplay() {
+		Timeline oneSec = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+			@SuppressWarnings("deprecation")
+			@Override
+			public void handle(ActionEvent event) {
+				//System.out.println(FpsDisplay);
+				//mScoreLabelOne.setText(Integer.toString(FpsDisplay));
+				FpsDisplay = 0;
+
+			}
+		}));
+		oneSec.setCycleCount(Animation.INDEFINITE);
+		oneSec.play();
+	}
+	
+	
+	
+	/**
+	 * 
+	 */
+	public void pauseGame() {
+		
+	}
+	
 	public void render() {
+		clearScreen();
 		mPlayerOnePaddle.setLayoutX(mPlayerOne.getPlayerPaddle().getXCord());
 		mPlayerOnePaddle.setLayoutY(mPlayerOne.getPlayerPaddle().getYCord());
 		mPlayerTwoPaddle.setLayoutX(mPlayerTwo.getPlayerPaddle().getXCord());
 		mPlayerTwoPaddle.setLayoutY(mPlayerTwo.getPlayerPaddle().getYCord());
 		mGameBall.setCenterX(mGameBall.getXLocation());
 		mGameBall.setCenterY(mGameBall.getYLocation());
+		mGC.fillOval(mGameBall.getCenterX(), mGameBall.getCenterY(), mGameBall.getRadius() * 2,
+				mGameBall.getRadius() * 2);
 	}
 	
 	/**
 	 * Resets the game ball to the middle of the screen.
+	 * This method is used for handling which player scored.
+	 * If the player is null then no player has scorred.
+	 * @param scoringPlayer The player who scored.
 	 */
 	public void resetGameBall(Player scoringPlayer) {
 		mGameBall.reset();
@@ -487,10 +382,135 @@ public final class GameController  {
 			mGameBall.changeBallVector(xVec, 0);
 		}
 		ANIMATION_SYSTEM.countdownAnimation(3, mGC);
-		mGC.fillOval(mGameBall.getCenterX(), mGameBall.getCenterY(), mGameBall.getRadius() * 2,
-				mGameBall.getRadius() * 2);
+		
 	}
 	
+	
+	
+	/**
+	 * Restarts game logic
+	 */
+	public void restartGame() {
+		stopGame();
+		resetGame();
+		startGame();
+	}
+	
+	/**
+	 * Resets game.
+	 */
+	public void resetGame() {
+		
+	}
+	
+	/**
+	 * Called after FXML initalization to pass scene to register listeners that can only be tagged onto the scene.
+	 * @param scene
+	 */
+	public void registerScene(Scene scene) {
+		KeyControl<KeyCode> playerOnePaddleUpControl = (KeyControl<KeyCode>)mPlayerOne.getPaddleUpControl();
+		KeyControl<KeyCode> playerOnePaddleDownControl = (KeyControl<KeyCode>)mPlayerOne.getPaddleDownControl();
+		KeyControl<KeyCode> playerTwoPaddleUpControl = (KeyControl<KeyCode>)mPlayerTwo.getPaddleUpControl();
+		KeyControl<KeyCode> playerTwoPaddleDownControl = (KeyControl<KeyCode>)mPlayerTwo.getPaddleDownControl();
+		mScene = scene;
+		mScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			
+			public void handle(KeyEvent event) {
+				System.out.println(event.getCharacter());
+				if (event.getCode().equals(playerOnePaddleUpControl.getControl())) {
+					if (mPlayerOneUp != true) {
+						playerOnePaddleUpControl.sendControlActivatedEvent(playerOnePaddleUpControl.getControl(), System.currentTimeMillis());
+						mPlayerOneUp = true;
+					}
+
+				} else if (event.getCode().equals(playerOnePaddleDownControl.getControl())) {
+					if (mPlayerOneDown != true) {
+						playerOnePaddleDownControl.sendControlActivatedEvent(playerOnePaddleDownControl.getControl(), System.currentTimeMillis());
+						mPlayerOneDown = true;
+					}
+
+				}
+				if (event.getCode().equals(playerTwoPaddleUpControl.getControl())) {
+					if (mPlayerTwoUp != true) {
+						playerTwoPaddleUpControl.sendControlActivatedEvent(playerTwoPaddleUpControl.getControl(), System.currentTimeMillis());
+						mPlayerTwoUp = true;
+					}
+
+				} else if (event.getCode().equals(playerTwoPaddleDownControl.getControl())) {
+					if (mPlayerTwoDown != true) {
+						playerTwoPaddleDownControl.sendControlActivatedEvent(playerTwoPaddleDownControl.getControl(), System.currentTimeMillis());
+						mPlayerTwoDown = true;
+					}
+				}
+			}
+		});
+		
+		mScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent event) {
+				if (event.getCode().equals(playerOnePaddleUpControl.getControl())) {
+					mPlayerOneUp = false;
+				} else if (event.getCode().equals(playerOnePaddleDownControl.getControl())) {
+					mPlayerOneDown = false;
+				}
+				if (event.getCode().equals(playerTwoPaddleUpControl.getControl())) {
+					mPlayerTwoUp = false;
+				} else if (event.getCode().equals(playerTwoPaddleDownControl.getControl())) {
+					mPlayerTwoDown = false;
+				}
+			}
+		});
+	}
+	
+	/**
+	 * Initializes the updater.
+	 */
+	public void updaterInit() {
+		
+		mGameLogicUpdater = new Timeline(
+				new KeyFrame(Duration.seconds(FRAME_PER_SECOND), new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent e) {
+						render();
+						if (mGameBall.isCanMove() == true) {
+							mGameBall.constantMove(mGameBall.getCenterX(), mGameBall.getCenterY(), mGC);
+							mGameBall.setCenterX(mGameBall.getXLocation());
+							mGameBall.setCenterY(mGameBall.getYLocation());
+							
+						}
+						checkCollisions();
+						if (mPlayerOneUp && mPlayerOne.getPlayerPaddle().canMoveUp()) {
+							mPlayerOne.getPlayerPaddle().moveUp();
+						} else {
+							mPlayerOneUp = false;
+						}
+						if (mPlayerOneDown  && mPlayerOne.getPlayerPaddle().canMoveDown()) {
+							mPlayerOne.getPlayerPaddle().moveDown();
+						} else {
+							mPlayerOneDown = false;
+						}
+						if (mPlayerTwoUp && mPlayerTwo.getPlayerPaddle().canMoveUp()) {
+							mPlayerTwo.getPlayerPaddle().moveUp();
+						} else {
+							mPlayerTwoUp = false;
+
+						}
+						if (mPlayerTwoDown && mPlayerTwo.getPlayerPaddle().canMoveDown()) {
+							mPlayerTwo.getPlayerPaddle().moveDown();
+						} else {
+							mPlayerTwoDown = false;
+						}
+
+						
+						FpsDisplay += 1;
+					}
+				}));
+		mGameLogicUpdater.setCycleCount(Animation.INDEFINITE);
+	}
+	
+	
+	/**
+	 * Starts the game logic. Can only be called if the came is not running. Otherwise it is ignored.
+	 */
 	public void startGame() {
 		if (mGameLogicUpdater != null) {
 			if (!mGameLogicUpdater.getStatus().equals(Status.RUNNING)) {
@@ -501,6 +521,9 @@ public final class GameController  {
 		}
 	}
 	
+	/**
+	 * Stops the game logic. Can only be called if the game is running. Otherwise it is ignored.
+	 */
 	public void stopGame() {
 		if (mGameLogicUpdater != null) {
 			if (mGameLogicUpdater.getStatus().equals(Status.RUNNING)) {
@@ -509,12 +532,10 @@ public final class GameController  {
 		}
 	}
 	
-	public void pauseGame() {
-		
-	}
+	
 	
 	/**
-	 * 
+	 * Class that implements action events to handle the logic of the click of a button for the restart menu item.
 	 * @author Jake Botka
 	 *
 	 */
@@ -522,13 +543,13 @@ public final class GameController  {
 		private static final long serialVersionUID = 5810132805011832855L;
 		
 		public void handle(ActionEvent event) {
-			
+			restartGame();
 		}
 		
 	}
 	
 	/**
-	 * 
+	 * Class that implements action events to handle the logic of the click of a button for the exit menu item.
 	 * @author Jake Botka
 	 *
 	 */
